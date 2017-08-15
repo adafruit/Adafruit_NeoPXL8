72fe79724094df75d5c8e0bfdc52ced4e6a13296
Phillip Burgess <paintyourdragon@dslextreme.com>
2017-08-14 21:31:29 -0700

    Changed strandtest to help identify LED output pins

--- examples/strandtest/strandtest.ino
+++ examples/strandtest/strandtest.ino
@@ -1,4 +1,5 @@
-// This is a PARED-DOWN NeoPixel example for the Adafruit_NeoPXL8 library.
+// This is a single NeoPixel example for the Adafruit_NeoPXL8 library,
+// tests LEDs and helps identify which output pin to which strip.
 // For more complete examples of NeoPixel operations, see the examples
 // included with the 'regular' Adafruit_NeoPixel library.
 
@@ -25,27 +26,44 @@ void setup() {
   strip.setBrightness(32);
 }
 
-uint8_t j;
+uint8_t frame = 0;
 
 void loop() {
-  for(int i=0; i< strip.numPixels(); i++) {
-    strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+  for(uint8_t r=0; r<8; r++) { // For each row...
+    for(int p=0; p<NUM_LED; p++) { // For each pixel of row...
+      strip.setPixelColor(r * NUM_LED + p, rain(r, p));
+    }
   }
   strip.show();
-  delay(1);
-  j++;
+  frame++;
 }
 
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+uint8_t colors[8][3] = { // RGB colors for the 8 rows...
+  255,   0,   0, // Row 0: Red
+  255, 160,   0, // Row 1: Orange
+  255, 255,   0, // Row 2: Yellow
+    0, 255,   0, // Row 3: Green
+    0, 255, 255, // Row 4: Cyan
+    0,   0, 255, // Row 5: Blue
+  192,   0, 255, // Row 6: Purple
+  255,   0, 255  // Row 7: Magenta
+};
+
+// Gamma-correction table improves the appearance of midrange colors
+#define _GAMMA_ 2.6
+const int _GBASE_ = __COUNTER__ + 1; // Index of 1st __COUNTER__ ref below
+#define _G1_ pow((__COUNTER__ - _GBASE_) / 255.0, _GAMMA_) * 255.0 + 0.5,
+#define _G2_ _G1_ _G1_ _G1_ _G1_ _G1_ _G1_ _G1_ _G1_ // Expands to 8 lines
+#define _G3_ _G2_ _G2_ _G2_ _G2_ _G2_ _G2_ _G2_ _G2_ // Expands to 64 lines
+const uint8_t gamma8[] = { _G3_ _G3_ _G3_ _G3_ };    // 256 lines
+
+// Given row number (0-7) and pixel number along row (0 - (NUM_LED-1)),
+// first calculate brightness (b) of pixel, then multiply row color by
+// this and run it through gamma-correction table.
+uint32_t rain(uint8_t row, int pixelNum) {
+  uint16_t b = 256 - ((frame - row * 32 + pixelNum * 256 / NUM_LED) & 0xFF);
+  return ((uint32_t)gamma8[(colors[row][0] * b) >> 8] << 16) |
+         ((uint32_t)gamma8[(colors[row][1] * b) >> 8] <<  8) |
+                    gamma8[(colors[row][2] * b) >> 8];
 }
 
