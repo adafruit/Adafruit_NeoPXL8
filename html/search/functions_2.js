b1018ba39c489c2b2be067d15f4ad27c3b034d3b
Doxygen CI <ci-arduino@invalid>
2023-05-19 21:15:41 +0000

    Deploy docs to GitHub Pages from commit 5f09a9b8ba
    Commit: 5f09a9b8ba095c736eb67359027cb637e3221898
    GitHub Actions run: 5028272248

--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['calc_5fgamma_5ftable',['calc_gamma_table',['../class_adafruit___neo_p_x_l8_h_d_r.html#afc4a2f0d55b16a59f51e2ff95e95b39d',1,'Adafruit_NeoPXL8HDR']]],
   ['canshow',['canShow',['../class_adafruit___neo_p_x_l8.html#ab932c13f23a4f2a2994c69f7e8829f8d',1,'Adafruit_NeoPXL8::canShow()'],['../class_adafruit___neo_p_x_l8_h_d_r.html#ac334c53854eb55531fca2f5f58e29eae',1,'Adafruit_NeoPXL8HDR::canShow()']]],
-  ['canstage',['canStage',['../class_adafruit___neo_p_x_l8.html#a7d2987dbb60a4042d823b32949740152',1,'Adafruit_NeoPXL8::canStage()'],['../class_adafruit___neo_p_x_l8_h_d_r.html#a4ba6e90e645733d29f7de770cd852344',1,'Adafruit_NeoPXL8HDR::canStage()']]]
+  ['canstage',['canStage',['../class_adafruit___neo_p_x_l8.html#a7d2987dbb60a4042d823b32949740152',1,'Adafruit_NeoPXL8::canStage()'],['../class_adafruit___neo_p_x_l8_h_d_r.html#a4ba6e90e645733d29f7de770cd852344',1,'Adafruit_NeoPXL8HDR::canStage()']]],
+  ['clear',['clear',['../class_adafruit___neo_p_x_l8_h_d_r.html#a5511eebc38a3c76144e9f4ed4c1d378d',1,'Adafruit_NeoPXL8HDR']]]
 ];
